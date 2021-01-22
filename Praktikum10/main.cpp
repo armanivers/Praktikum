3f79fe764eae829163c3395a9fc2e2c037e0da5a
Arii <armanivers@hotmail.com>
2021-01-22 18:51:11 +0100

    + praktikum 10 mit zusatzlich uberladene Operatoren

--- Praktikum10/main.cpp
+++ Praktikum10/main.cpp
@@ -5,14 +5,36 @@ int main()
 {
     using namespace fhdo_pk2;
 
-    Matrix<int> m(2);
+    Matrix<int> m1(2);
 
-    m.set(0,0,1);
-    m.set(0,1,2);
-    m.set(1,0,3);
-    m.set(1,1,4);
+    m1.set(0,0,1);
+    m1.set(0,1,2);
+    m1.set(1,0,3);
+    m1.set(1,1,4);
 
-    std::cout << m << "*" << std::endl << m << "=" << std::endl << m*m;
+    Matrix<int> m2 = m1;
 
+    // Kopy Konstruktor Test
+    std::cout << m1 << std::endl;
+    std::cout << m2 << std::endl;
+
+    // Multiplikation
+    std::cout << m1 << "*" << std::endl << m1 << "=" << std::endl << m1*m1;
+
+    // Addition
+    //std::cout << m1 << "+" << std::endl << m1 << "=" << std::endl << m1+m1;
+
+    // Subtraktion
+    //std::cout << m1 << "-" << std::endl << m1 << "=" << std::endl << m1-m1;
+
+    // Power of (^)
+    //std::cout << m1 << "power of 0 is " << std::endl << (m1^0) << std::endl;
+
+    // "==" operator (equals)
+    //std::cout << m1 << "-" << std::endl << m2 << "are equal? " << (m1==m2) << std::endl;
+    
+    // "-" operator (Negation)
+    //std::cout << m1 << "negated is " << std::endl << (-m1) << std::endl;
+    
     return 0;
 }
--- Praktikum10/matrix.h
+++ Praktikum10/matrix.h
@@ -1,6 +1,7 @@
 #ifndef _MATRIX_H
 #define _MATRIX_H
-#include <ostream>
+#include <iostream>
+
 namespace fhdo_pk2{
 
         template<class T>
@@ -10,7 +11,6 @@ namespace fhdo_pk2{
         int n;
         T **m;
 
-
         public:
         Matrix(int n);
         ~Matrix();
@@ -18,13 +18,27 @@ namespace fhdo_pk2{
 
         void set(int zeile, int spalte,T zahl);
 
-        void init();
+        void initZero();
 
         // Operatoren
         // braucht man das nicht?
         //Matrix<T> operator*(const Matrix<T> &m1,const Matrix<T> &m2);
+        
+
+        // muss const sein!
+        T* operator[](int index) const
+        {
+            if(index < 0 || index > this->getDimension())
+            {
+                return nullptr;
+            }
+
+            return m[index];
+        }
 
         // Getter/Setter
+
+        // warum muss das unbedingt const sein? (sonst Fehler bei operator* wo ich getDimension() Aufrufe)
         inline int getDimension() const
         {
             return n;
@@ -39,6 +53,7 @@ namespace fhdo_pk2{
     template<class T>
     Matrix<T>::Matrix(int n) : n{n}
     {   
+        std::cout << "Konstruktor" << std::endl;
         // Zeile
         // M zeigt auf ein T Zeiger Array
         // das T* braucht man, weil m immernoch ein doppelzeiger ist, und T nur ein Zeiger (Konvertierung von ** zu *)
@@ -52,11 +67,12 @@ namespace fhdo_pk2{
         }
 
         // Matrix zahlen auf 0 setzen;
-        //init();
+        initZero();
     }
 
+    // Hilfsmethode um Matrix mit 0 initialisieren
     template<class T>
-    void Matrix<T>::init()
+    void Matrix<T>::initZero()
     {
         for(int z = 0; z < n; z++)
         {
@@ -70,6 +86,7 @@ namespace fhdo_pk2{
     template<class T>
     Matrix<T>::~Matrix()
     {
+        std::cout << "Destruktor" << std::endl;
         for(int i = 0; i < n ; i++)
         {
             delete[](m[i]);
@@ -82,6 +99,8 @@ namespace fhdo_pk2{
     template<class T>
     Matrix<T>::Matrix(const Matrix& orig) : Matrix(orig.getDimension())
     {
+        std::cout << "Copy Konstruktor" << std::endl;
+
         int dimension = orig.getDimension();
 
         for(int z = 0; z < dimension; z++)
@@ -119,10 +138,17 @@ namespace fhdo_pk2{
         {
             for(int s = 0; s < n; s++)
             {
-                result.getMatrix(z)[s] = 0;
+                // Falls Matrix nicht mit 0 initialisiert!
+                //result[z][s] = 0;
+
+                // Alternative ohne Uberladung von []
+                //result.getMatrix(z)[s] = 0;
                 for(int i = 0; i < n; i++)
                 {
-                    result.getMatrix(z)[s] += m1.getMatrix(z)[i] * m2.getMatrix(i)[s];
+                    result[z][s] += m1[z][i] * m2[i][s];
+                    
+                    // Alternative ohne Uberladung von []
+                    //result.getMatrix(z)[s] += m1.getMatrix(z)[i] * m2.getMatrix(i)[s];
                 }
             }
         }
@@ -137,14 +163,131 @@ namespace fhdo_pk2{
 
         for(int z = 0; z < n; z++)
         {
+            ausgabe << "|";
             for(int s = 0; s < n; s++)
             {
-                ausgabe << m.getMatrix(z)[s] << " ";
+                ausgabe << m[z][s] << " ";
+
+                // Alternative ohne Uberladung von []
+                //ausgabe << m.getMatrix(z)[s] << " ";
             }
-            ausgabe << std::endl;
+            ausgabe << "|" <<std::endl;
         }
 
         return ausgabe;
     }
+
+    template<class T>
+    Matrix<T> operator+(const Matrix<T> &m1,const Matrix<T> &m2)
+    {
+        if(m1.getDimension() != m2.getDimension())
+        {
+            // return zero matrix
+        }
+
+        int n = m1.getDimension();
+
+        Matrix<T> result(n);
+
+        for(int z = 0; z < n; z++)
+        {
+            for(int s = 0; s < n; s++)
+            {
+                result[z][s] = m1[z][s] + m2[z][s];
+            }
+        }
+
+        return result;
+    }
+
+    template<class T>
+    Matrix<T> operator-(const Matrix<T> &m1, const Matrix<T> &m2)
+    {
+        if(m1.getDimension() != m2.getDimension())
+        {
+            // return zero matrix
+        }
+
+        int n = m1.getDimension();
+
+        Matrix<T> result(n);
+
+        for(int z = 0; z < n; z++)
+        {
+            for(int s = 0; s < n; s++)
+            {
+                result[z][s] = m1[z][s] - m2[z][s];
+            }
+        }
+
+        return result;
+    }
+
+    
+    template<class T>
+    Matrix<T> operator^(Matrix<T> &m,int number)
+    {
+        if(number < 0) return m;
+
+        // return zero matrix;
+        if(number == 0) return Matrix<T> (m.getDimension());
+
+        int n = m.getDimension();
+
+        for(int z = 0; z < n; z++)
+        {
+            for(int s = 0; s < n; s++)
+            {
+                // power of 
+                for(int i = 0; i < number-1; i++)
+                {
+                     m[z][s] *=m[z][s];
+                }
+            }
+        }
+
+        return m;
+    }
+    
+
+    template<class T>
+    bool operator==(const Matrix<T> &m1, const Matrix<T> &m2)
+    {
+        if(m1.getDimension() != m2.getDimension())
+        {
+            return false;
+        }
+
+        int n = m1.getDimension();
+
+        for(int z = 0; z < n; z++)
+        {
+            for(int s = 0; s < n; s++)
+                {
+                    if(m1[z][s] != m2[z][s])
+                    {
+                        return false;
+                    }
+                }   
+        }
+        return true;
+    }
+
+    template<class T>
+    Matrix<T> operator-(Matrix<T> &m)
+    {
+        int n = m.getDimension();
+
+        for(int z = 0; z < n; z++)
+        {
+            for(int s = 0; s < n; s++)
+            {
+                m[z][s] *= -1;
+            }   
+        }
+    
+        return m;
+    }
+    
 }
 #endif
